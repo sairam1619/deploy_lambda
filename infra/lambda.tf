b0d92a74cb9123edfb1e3b6acb177b5b71758cc7
sairam619 <sairamgangapuram2001@gmail.com>
2026-05-27 19:54:16 +0530

    changes done in yml file to manually run the apply

--- infra/lambda.tf
+++ infra/lambda.tf
@@ -20,8 +20,8 @@ resource "aws_lambda_function" "lambda_function" {
   function_name = var.lambda_function_name
 
   role = var.lambda_role_arn
-  
-  timeout = 895
+
+  timeout = 900
 
   handler = "app.lambda_handler"
 
